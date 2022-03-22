f2a8d2c5bb328960628cdf5804c2a07ceba9416d
Tona <tonabrix1@yahoo.com>
2022-03-22 19:14:17 -0400

    corrected some comments

--- rustml/src/main.rs
+++ rustml/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     //planning to do mnist character recognition to build this around, so I will be using 28x28 images
     let input_layer = 28*28;
     let hidden_layer1 = 128;
+    create_layer(1,3);
     //since there are 10 classes (0-9) the ai will print a "confidence matrix" that should sum to 1
     let output_layer = 10;
     //28*28x128x10 neural network is created
@@ -25,10 +26,13 @@ fn create_network(dim : Vec<(usize,usize)>) -> Vec<Array2<f32>>{
     return network;
 }
 
-// inner : the dimension of input matrix-a
-// outer : the dimensions of input matrix-b
+// len2D : layer.len() == len2D
+// len1D : layer[n].len() == len1D
 // out : a 2D array (ndarray::Array2) of the connections between matrix-a and matrix-b
-fn create_layer(inner : usize, outer : usize) -> Array2<f32> {
+fn create_layer(len2D : usize, len1D : usize) -> Array2<f32> {
+    //initializes an array of 0's: inner=1; outer=3
+    // [[0, 0, 0]]
     let out = Array2::<f32>::zeros((inner,outer));
+    println!("{}",out);
     return out;
 }
